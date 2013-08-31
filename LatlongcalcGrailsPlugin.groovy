253ea8998424a7bfc0de4bcc9fdc5c946043b606
Vahid <mx1@mx1-DP.(none)>
2013-08-31 16:32:45 +0100

    first push

---
+++ LatlongcalcGrailsPlugin.groovy
@@ -0,0 +1,12 @@
+class LatlongcalcGrailsPlugin {
+	def version = "0.1"
+	def grailsVersion = "1.0 > *"
+	def title = "Latitude Longitude Range calculator"
+	def author = "Vahid Hedayati"
+	def authorEmail = "badvad@gmail.com"
+	def description = 'Grails plugin which given a range a longitude and latitude value will retun min/max values of longitude and latitude'
+	def documentation = "http://grails.org/plugin/latlongcalc"
+	def license = "GPL2"
+	def issueManagement = [system: 'GITHUB', url: 'https://github.com/vahidhedayati/latlongcalc/issues']
+	def scm = [url: 'https://github.com/vahidhedayati/latlongcalc']
+}
